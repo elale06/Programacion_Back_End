e312fd2aabedbaa9d90578d95568e7658441aca4
elale066 <166960616+elale06@users.noreply.github.com>
2026-08-18 11:49:29 -0400

    Delete ejemplo directory

--- ejemplo/ejemplo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from primeraApp import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.inicio),
-    path('ahora/', views.ahora),
-]
--- ejemplo/primeraApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrimeraappConfig(AppConfig):
-    name = 'primeraApp'
